8b4c5d3c45ef92c9a83981b3b42b9903515ccada
ikranhaji <ikranhaji4@gmail.com>
2023-02-13 17:29:31 -0600

    Problem 47

--- problems/problem_047.py
+++ problems/problem_047.py
@@ -13,4 +13,24 @@
 # like ".isalpha", ".isdigit", ".isupper", and ".islower"
 
 def check_password(password):
-    pass
+    if len(password) < 6 or len(password) > 12:
+        return False
+    has_lower = False
+    has_upper = False
+    has_digit = False
+    has_special_char = False
+
+    for char in password:
+        if char.islower():
+            has_lower = True
+        elif char.isdigit():
+            has_digit = True
+        elif char.isupper():
+            has_upper = True
+        elif char == "$" or char == "!" or char == "@":
+            has_special_char = True
+
+    if has_special_char and has_digit and has_lower and has_upper:
+        return True
+    else:
+        return False
