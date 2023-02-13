bd511d948cf78db288b28bf9f5b061a5c2dcf607
ikranhaji <ikranhaji4@gmail.com>
2023-02-13 11:55:55 -0600

    problem 69

--- problems/problem_069.py
+++ problems/problem_069.py
@@ -1,7 +1,7 @@
 # Write a class that meets these requirements.
 #
 # Name:       Student
-#
+
 # Required state:
 #    * name, a string
 #
@@ -23,15 +23,26 @@
 
 
 # class Student
+class Student:
     # method initializer with required state "name"
+    def __init__(self, name):
         # self.name = name
+        self.name = name
         # self.scores = [] because its an internal state
+        self.scores = []
 
     # method add_score(self, score)
+    def add_score(self, score):
         # appends the score value to self.scores
+        return self.scores.append(score)
 
     # method get_average(self)
+    def get_average(self):
         # if there are no scores in self.scores
+        if len(self.scores) == 0:
             # return None
+            return None
         # returns the sum of the scores divided by
         # the number of scores
+        else:
+            return sum(self.scores) / len(self.scores)
