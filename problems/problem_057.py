4622a044471c62da64f8e882bda4b63c5790e4fd
ikranhaji <ikranhaji4@gmail.com>
2023-02-13 18:02:35 -0600

    Problem 57

--- problems/problem_057.py
+++ problems/problem_057.py
@@ -13,3 +13,10 @@
 #     * input:   3
 #       returns: 1/2 + 2/3 + 3/4
 
+
+def sum_fraction_sequence(n):
+    sum = 0
+    # create the range based on n
+    for i in range(1, n + 1):
+        sum += i / (i + 1)
+    return sum
